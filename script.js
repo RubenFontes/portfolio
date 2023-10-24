48fc938b8f81d437dfe14f1631d4bc92169720b8
Ruben Fontes <55508678+RubenFontes@users.noreply.github.com>
2023-10-24 12:04:55 -0300

    create function: handleAnimation

--- script.js
+++ script.js
@@ -11,4 +11,23 @@ for (let i = 1; i <= 11; i++) {
       figcaption.style.display = "block";
     });
   }
-  
+
+// Animação
+const elementos = document.querySelectorAll(".animacao");
+
+function handleAnimation() {
+  elementos.forEach((elemento) => {
+    const rect = elemento.getBoundingClientRect();
+    if (elemento.getBoundingClientRect().top < window.innerHeight && rect.bottom >= 0) {
+      elemento.classList.add("animated");
+    } else {
+      elemento.classList.remove("animated");
+    }
+  });
+}
+
+// Adiciona a função de manipulação de eventos para o evento "load"
+window.addEventListener("load", handleAnimation);
+
+// Adiciona a mesma função de manipulação de eventos para o evento "scroll"
+window.addEventListener("scroll", handleAnimation);
